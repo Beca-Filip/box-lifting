1968caa78d9b0029f4891db61701d6c102dd1d4d
Filip <becaphilippe@gmail.com>
2023-02-16 11:00:06 +0100

    transporting wrenches

---
+++ utils/markers_and_forceplate_struct/WrenchesRotateTranslate.m
@@ -0,0 +1,16 @@
+function [f_grf_trans] = WrenchesRotateTranslate(R,p,f_grf)
+%WRENCHESTRANSLATEROTATE first rotates then rotates 6DoF wrenches.
+
+% Reshape the position vector
+p = reshape(p, [1, 3]);
+skewP = [[0, -p(3), p(2)]; [p(3), 0, -p(1)]; [-p(2), p(1), 0]];
+
+% Reassign
+f_grf_trans = f_grf;
+% Rotate both forces and moments
+f_grf_trans(1:3, :) = R * f_grf_trans(1:3, :);
+f_grf_trans(4:6, :) = R * f_grf_trans(4:6, :);
+% Transport moment
+f_grf_trans(4:6, :) = f_grf_trans(4:6, :) + skewP * f_grf_trans(1:3, :);
+end
+
