b631117b835257d34c465ce33e11402de6d9eb98
Filip <becaphilippe@gmail.com>
2023-02-22 18:28:25 +0100

    Took out reasignement cuz it didn't dereference the original casadi MX

--- utils/markers_and_forceplate_struct/WrenchesRotateTranslate.m
+++ utils/markers_and_forceplate_struct/WrenchesRotateTranslate.m
@@ -5,11 +5,8 @@ function [f_grf_trans] = WrenchesRotateTranslate(R,p,f_grf)
 p = reshape(p, [1, 3]);
 skewP = [[0, -p(3), p(2)]; [p(3), 0, -p(1)]; [-p(2), p(1), 0]];
 
-% Reassign
-f_grf_trans = f_grf;
 % Rotate both forces and moments
-f_grf_trans(1:3, :) = R * f_grf_trans(1:3, :);
-f_grf_trans(4:6, :) = R * f_grf_trans(4:6, :);
+f_grf_trans = [R * f_grf(1:3, :); R * f_grf(4:6, :)];
 % Transport moment
 f_grf_trans(4:6, :) = f_grf_trans(4:6, :) + skewP * f_grf_trans(1:3, :);
 end
