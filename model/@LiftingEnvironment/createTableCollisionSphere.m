b901f044667cbd132b5eac5a84c479e1c7a6b3a1
Filip <becaphilippe@gmail.com>
2023-02-23 17:05:18 +0100

    5 table spheres to represent the surface

--- model/@LiftingEnvironment/createTableCollisionSphere.m
+++ model/@LiftingEnvironment/createTableCollisionSphere.m
@@ -1,11 +1,24 @@
-function sphere = createTableCollisionSphere(obj)
+function spheres = createTableCollisionSphere(obj)
 
-name = "Table sphere";
+% Same for all spheres
 rigidlink = 0;
 frame = 0;
-pos = obj.TableCenterCoordinates;
-radius = max(obj.TableWidth/2, obj.TableHeight/2);
 
+% Spheres array
+spheres = [];
 
-sphere = CollisionSphere6DOF(name,rigidlink,frame,pos,radius);
+% Radius of the spheres is fixed to width/10
+radius = obj.TableWidth / 10;
+
+% Table upper corner
+TableUpperCornerCoordinates = obj.TableCenterCoordinates + [-obj.TableWidth/2; obj.TableHeight/2];
+
+% Create five spheres along the upper edge of the table
+for nSph = 1 : 5
+    name = sprintf("Upper-edge table sphere %d", nSph);
+    sphCenter = TableUpperCornerCoordinates + [(nSph-1)*2*radius + radius; -radius];
+    sphCenter = [sphCenter; 0];
+    spheres = [spheres, CollisionSphere6DOF(name,rigidlink,frame,sphCenter,radius)];
+end
+
 end
