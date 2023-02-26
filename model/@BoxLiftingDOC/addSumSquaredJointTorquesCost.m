c2aa532b945b1e076ea64b95264980d0c24acb3d
Filip Becanovic <becaphilippe@gmail.com>
2023-02-27 00:08:19 +0100

    joint torques are now normalized by gravity in cost function

--- model/@BoxLiftingDOC/addSumSquaredJointTorquesCost.m
+++ model/@BoxLiftingDOC/addSumSquaredJointTorquesCost.m
@@ -1,7 +1,7 @@
 function obj = addSumSquaredJointTorquesCost(obj)
 
 % Cost:
-obj.sumSquaredJointTorques = sum(sum(obj.tau.^2, 2) ./ obj.splineEvaluationNumber) ./ obj.splineDimension;
+obj.sumSquaredJointTorques = sum(sum((obj.tau ./ abs(obj.casadiHumanModel.Gravity)).^2, 2) ./ obj.splineEvaluationNumber) ./ obj.splineDimension;
 
 % Add the cost function to the vector:
 obj.costFunctionVector = vertcat(obj.costFunctionVector, obj.sumSquaredJointTorques);
