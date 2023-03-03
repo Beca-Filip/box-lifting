721e81c94201a54385031b468ea138b374ae93d9
Filip <becaphilippe@gmail.com>
2023-03-03 15:17:00 +0100

    added default values so it could be initialized with no parameters

--- model/@InitialGuessDOC/InitialGuessDOC.m
+++ model/@InitialGuessDOC/InitialGuessDOC.m
@@ -3,11 +3,11 @@ classdef InitialGuessDOC < handle
     
     properties
         % Number of trials
-        nTrials(1, 1)
+        nTrials(1, 1) = 1
         % Size of primal solution
-        nPrimal(1, 1)
+        nPrimal(1, 1) = 60
         % Size of dual solution
-        nDual(1, 1)
+        nDual(1, 1) = 3000
         % Guess for primal solution
         primal
         % Guess for dual solution
@@ -16,12 +16,18 @@ classdef InitialGuessDOC < handle
     
     methods
         function obj = InitialGuessDOC(nTrials, nPrimal, nDual)
-            % Get the numer of trials
-            obj.nTrials = nTrials;
-            % Initialize the size of primal solution
-            obj.nPrimal = nPrimal;
-            % Initialize the size of dual solution
-            obj.nDual = nDual;
+            if nargin < 3
+                obj.nTrials = 1;
+                obj.nPrimal = 60;
+                obj.nDual = 3000;
+            else
+                % Get the numer of trials
+                obj.nTrials = nTrials;
+                % Initialize the size of primal solution
+                obj.nPrimal = nPrimal;
+                % Initialize the size of dual solution
+                obj.nDual = nDual;
+            end
             
             % Initialize the primal solution
             obj.primal = zeros(obj.nPrimal, obj.nTrials);
