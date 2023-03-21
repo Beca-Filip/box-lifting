6b62cea279ce13ac733c96e9a9c164c5fa8b2576
Filip <becaphilippe@gmail.com>
2023-03-21 16:05:52 +0100

    changed printToFile for LiftingEnvironment

--- model/@LiftingEnvironment/printToFile.m
+++ model/@LiftingEnvironment/printToFile.m
@@ -1,10 +1,9 @@
 function printToFile(obj, filename)
 
-% Open file
-fileID = fopen(filename, 'wt');
-
 % All properties of the obj
 PropertyNames = properties(obj);
+% All vars
+var = cell(length(PropertyNames), 1);
 % Go through each property
 for ii = 1 : length(PropertyNames)
     
@@ -14,24 +13,13 @@ for ii = 1 : length(PropertyNames)
     % Get the property
     property = obj.(propertyname);
     
-    % Get the length of the property
-    propertylength = length(property);
-    
-    % Get string format for property
-    if propertylength > 1
-        stringformat = "%s: %f" + join(repmat(", %f", [1, propertylength-1]));
-    else
-        stringformat = "%s: %f";
-    end
-    
-    % Get whole string for property
-    stringproperty = sprintf(stringformat, propertyname, property);
-    
-    % Print to file
-    fprintf(fileID, stringproperty + "\n");
+    % Set var
+    var{ii} = reshape(property, 1, []);    
 end
 
-% Close file
-fclose(fileID);
+% Create table
+t = table(var{:}, 'VariableNames', PropertyNames);
+% Write table to file
+writetable(t, filename, 'WriteRowNames', false);
 
 end
